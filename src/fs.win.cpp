349f4074435f325d251917d5ca7e0e227daaf124
Jian Chen <admin@chensoft.com>
2018-08-06 15:13:07 +0800

    cmake: provide cmake files

---
+++ src/fs.win.cpp
@@ -0,0 +1,7 @@
+/**
+ * Created by Jian Chen
+ * @since  2018.08.06
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://chensoft.com
+ */
+ 
---
+++ test/catch.cpp
@@ -0,0 +1,15 @@
+/**
+ * Created by Jian Chen
+ * @since  2018.08.06
+ * @author Jian Chen <admin@chensoft.com>
+ * @link   http://chensoft.com
+ */
+#define CATCH_CONFIG_RUNNER
+
+#include "fs/fs.hpp"
+#include "catch.hpp"
+
+int main(int argc, char *argv[])
+{
+    return Catch::Session().run(argc, argv);
+}
